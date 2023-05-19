5e9d18e2f54c2e8ef75e4557c784847c477e7c7e
Yahya305 <saimyahya47@gmail.com>
2023-05-19 20:18:05 +0500

    xx

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const  connectMongo= require("../db")
+const  connectMongo= require("./db")
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -11,19 +11,22 @@ const port = process.env.PORT || 5000
 // }))
 const BASE_URL=process.env.BASE_URL
 // const allowedOrigins = ['http://localhost:3000', 'http://192.168.18.54:3000'];
-const allowedOrigins = ['http://localhost:3000', 'http://192.168.18.54:3000',BASE_URL];
+const allowedOrigins = ['http://localhost:3000', 'http://192.168.18.54:3000'];
 const corsOptions = {
   origin: function (origin, callback) {
+    console.log(origin)
     // check if the origin is allowed
     if (!origin) {
       // if no origin provided, allow the request
       return callback(null, true);
     }
     if (allowedOrigins.indexOf(origin) === -1) {
+      console.log("Not Allowed")
       // if the origin is not allowed, reject the request
       return callback(new Error('Not allowed by CORS'));
     }
     // if the origin is allowed, allow the request
+    console.log("Origin is allowed")
     return callback(null, true);
   }
 };
@@ -32,10 +35,10 @@ connectMongo(app,port);
 
 
 app.use(express.json())
-app.use('/api/randomquote',require('../routes/quote'));
-app.use('/api/notes',require('../routes/notes'))
-app.use('/api/auth',require('../routes/auth'))
-app.use('/api/imagelab',require('../routes/imagelab'))
+app.use('/api/randomquote',require('./routes/quote'));
+app.use('/api/notes',require('./routes/notes'))
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/imagelab',require('./routes/imagelab'))
 
 
 // app.listen(port, () => {
